0bac195a21bb9e70de4fc4e7a6b063a240262a34
CatAvadani <catalinaavadani@gmail.com>
2024-11-19 20:09:59 +0100

    Setup basic routes

---
+++ app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="flex items-center justify-center py-10 bg-gray-200">
+      <p>by Catalina Avadani {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
