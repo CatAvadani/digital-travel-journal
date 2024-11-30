cbb7e3308569ad14828d9cda9f76fe2959a021bb
CatAvadani <catalinaavadani@gmail.com>
2024-11-30 21:37:04 +0100

    Update

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -27,11 +27,11 @@ const socialLinks = [
 export default function Footer() {
   return (
     <footer
-      className='absolute flex justify-between gap-8 items-center bottom-10 lg:bottom-10
+      className='absolute flex justify-between gap-1 sm:gap-8 items-center bottom-10 lg:bottom-10
     sm:left-24 md:left-32 z-50 text-white'
     >
       <div className='w-8 ml-4 lg:w-32 h-[0.1px] border-b border--white'></div>
-      <ul className='flex justify-center items-center gap-3 sm:gap-5  rounded-full'>
+      <ul className='flex justify-center items-center gap-2 sm:gap-5  rounded-full'>
         {socialLinks.map((link) => (
           <li key={link.href}>
             <Link
