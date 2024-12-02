3bf7e55bad1bc2326f6997b92b1c0f25df8cfb55
CatAvadani <catalinaavadani@gmail.com>
2024-12-02 22:37:28 +0100

    Refactor layout and styles for improved responsiveness and consistency

--- app/components/ui/LoadingSpinner.tsx
+++ app/components/ui/LoadingSpinner.tsx
@@ -2,7 +2,7 @@ import { RotatingLines } from 'react-loader-spinner';
 
 export default function LoadingSpinner() {
   return (
-    <div className='absolute inset-0 flex justify-center items-center loadingBackground z-50'>
+    <div className='absolute inset-0 flex justify-center items-center z-50'>
       <RotatingLines
         strokeColor='#B75E7C'
         strokeWidth='5'
--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
         />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen defaultBackground`}
       >
         <Providers>
           <ClientWrapper>
